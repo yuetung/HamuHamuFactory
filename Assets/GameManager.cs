cc93bbc15605feda7168e2eb9ce6fa6e6814117d
yuetung <limyuettung@hotmail.com>
2019-05-24 18:09:26 +0800

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public class Employee
+    {
+        string field;
+        int sprite;
+        string name;
+        List<int> stationStats;
+        int costPerDay;
+        public Employee(string field, int sprite, string name, List<int> stationStats, int costPerDay)
+        {
+            this.field = field;
+            this.name = name;
+            this.stationStats = stationStats;
+            this.costPerDay = costPerDay;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<Employee> UpdateHiring(string field, int num, int level, List<int> stations)
+    {
+        List<Employee> employees = new List<Employee>();
+        // TODO: do random generating
+        return employees;
+    }
+}
