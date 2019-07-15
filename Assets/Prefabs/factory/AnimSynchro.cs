559cc24b67cea62de3aeff621e3a7ebe9e181a7f
binaray@popon <raychengcx@live.com>
2019-07-15 19:01:01 +0800

    assets and prefabs

---
+++ Assets/Prefabs/factory/AnimSynchro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// for animation events: lights, sound, etc
+/// </summary>
+public class AnimSynchro : MonoBehaviour
+{
+    [SerializeField]
+    private List<Animator> childAnimators;
+
+    public bool isBlinking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBoolAnimEvent(bool x)
+    {
+        foreach (Animator anim in childAnimators)
+        {
+            //set this to a generalised parameter
+            anim.SetBool("isBlinking", x);
+        }
+    }
+}
---
+++ Assets/Prefabs/factory/FactoryEntity.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryEntity : MonoBehaviour
+{
+    [SerializeField]
+    private List<Animator> childAnimators;
+
+    //test variables: please delete and use function in future
+    public bool isStarted=false;
+    public bool isOperating = false;
+    public bool triggerAction = false;
+    bool prev = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsStarted(isStarted);
+        IsOperating(isOperating);
+        if (triggerAction != prev)
+        {
+            TriggerAction();
+            prev = triggerAction;
+        }
+    }
+    
+    public void IsStarted(bool x)
+    {
+        if (childAnimators != null)
+            foreach (Animator anim in childAnimators)
+            {
+                anim.SetBool("isStarted", x);
+            }
+    }
+
+    public void IsOperating(bool x)
+    {
+        if (childAnimators != null)
+            foreach (Animator anim in childAnimators)
+            {
+                anim.SetBool("isOperating", x);
+            }
+    }
+
+    public void TriggerAction()
+    {
+        if (childAnimators != null)
+            foreach (Animator anim in childAnimators)
+            {
+                anim.SetTrigger("triggerAction");
+            }
+    }
+}
