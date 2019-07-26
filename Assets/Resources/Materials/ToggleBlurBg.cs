57e5311f102a606a35e3e5060e57a84bff87e08a
binaray@popon <raychengcx@live.com>
2019-07-26 21:28:14 +0800

    ui and asset push

---
+++ Assets/Resources/Materials/ToggleBlurBg.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleBlurBg : MonoBehaviour
+{
+    void OnDisable()
+    {
+        Debug.Log("PrintOnDisable: script was disabled");
+        this.gameObject.transform.parent.GetComponent<RawImage>().enabled = false;
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("PrintOnEnable: script was enabled");
+        this.gameObject.transform.parent.GetComponent<RawImage>().enabled = true;
+    }
+}
