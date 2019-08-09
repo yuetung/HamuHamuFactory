a9c8d34b050c1217e1c16cc84d608dcee100aec4
guzhiyao <guzhiyao1234@gmail.com>
2019-08-09 13:56:10 +0800

    New Scenes: Beach, Park, and Market

---
+++ Assets/Scripts/CloudScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CloudScroll : MonoBehaviour
+{
+   
+    public float scroll_speed; 
+    RectTransform backgroundTs;
+    private GameObject clonedGameObj;
+    
+    private void Start() {
+        transform.position = new Vector3(0, transform.position.y);
+        clonedGameObj = Instantiate(GameObject.Find("cloud"));
+    }
+
+    void Update()
+    {
+        //clonedGameObj.transform.position = new Vector3(clonedGameObj.transform.position.x + scroll_speed, clonedGameObj.transform.position.y);
+        //transform.Translate(scroll_speed,0,0);
+    }
+}
---
+++ Assets/cloudSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject clonedGameObj;
+    private float backgroundWidth;
+    RectTransform backgroundTs;
+    public float scroll_speed; 
+    
+    void Start()
+    {
+        clonedGameObj = Instantiate(GameObject.Find("cloud"));
+        transform.position = new Vector3(0, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clonedGameObj.transform.position.x == backgroundWidth)
+        {
+            Destroy(clonedGameObj);
+        }
+    }
+}
