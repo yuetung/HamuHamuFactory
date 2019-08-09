7396f5015d135c389ffb791854ed5ddd594df375
guzhiyao <guzhiyao1234@gmail.com>
2019-08-09 14:38:30 +0800

    Beach Scene: Implemented Moving Clouds Spawner

--- Assets/Scripts/CloudScroll.cs
+++ Assets/Scripts/CloudScroll.cs
@@ -7,17 +7,17 @@ public class CloudScroll : MonoBehaviour
 {
    
     public float scroll_speed; 
-    RectTransform backgroundTs;
-    private GameObject clonedGameObj;
+    public float background_width;
     
     private void Start() {
-        transform.position = new Vector3(0, transform.position.y);
-        clonedGameObj = Instantiate(GameObject.Find("cloud"));
     }
 
     void Update()
     {
-        //clonedGameObj.transform.position = new Vector3(clonedGameObj.transform.position.x + scroll_speed, clonedGameObj.transform.position.y);
-        //transform.Translate(scroll_speed,0,0);
+        if(transform.position.x >= background_width){
+            Destroy(this.gameObject);
+            Debug.Log("cloud has been destroyed");
+        }
+        transform.Translate(scroll_speed,0,0);
     }
 }
--- Assets/cloudSpawner.cs
+++ Assets/cloudSpawner.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class cloudSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject clonedGameObj;
+    public GameObject gameObjectToMove;
+    public float waitTime;
+    public float scroll_speed; 
+    
     private float backgroundWidth;
     RectTransform backgroundTs;
-    public float scroll_speed; 
+
+    Vector3 starting_position;
     
     void Start()
     {
-        clonedGameObj = Instantiate(GameObject.Find("cloud"));
-        transform.position = new Vector3(0, transform.position.y);
+        backgroundTs= (RectTransform)GameObject.Find("background").transform;
+        backgroundWidth =  backgroundTs.rect.width;
+        Debug.Log("new cloud created");
+        StartCoroutine(SpawnForever(waitTime));
     }
 
     // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnForever(float waitTime)
     {
-        if (clonedGameObj.transform.position.x == backgroundWidth)
+        while (true)
         {
-            Destroy(clonedGameObj);
+            yield return new WaitForSeconds(waitTime);
+            starting_position = new Vector3(0, transform.position.y);
+            GameObject tempClone = Instantiate(gameObjectToMove,starting_position,this.transform.rotation,this.gameObject.transform);
+            tempClone.GetComponent<CloudScroll>().scroll_speed = scroll_speed;
+            tempClone.GetComponent<CloudScroll>().background_width = backgroundWidth;
         }
     }
 }
