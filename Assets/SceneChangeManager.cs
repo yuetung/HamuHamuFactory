f89f988be34cbb5d8de3ba226a094b94efed5b97
yuetung <limyuettung@hotmail.com>
2019-08-15 17:56:06 +0800

    final?

--- Assets/SceneChangeManager.cs
+++ Assets/SceneChangeManager.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HomepageButtons : MonoBehaviour
+public class SceneChangeManager : MonoBehaviour
 {
-    public GameObject continueButton;
-
     // Start is called before the first frame update
     void Start()
     {
-        if (!PlayerPrefs.HasKey("tuorialStage"))
-        {
-            continueButton.SetActive(false);
-        }
+        
     }
 
     // Update is called once per frame
@@ -22,14 +17,8 @@ public class HomepageButtons : MonoBehaviour
         
     }
 
-    public void ContinueGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("Factory", LoadSceneMode.Single);
-    }
-
-    public void NewGame()
-    {
-        PlayerPrefs.DeleteAll();
-        SceneManager.LoadScene("MV", LoadSceneMode.Single);
     }
 }
